cafd37493a8b03493829d5b2d2216ce3859d8718
Santiago <santimella11@gmail.com>
2026-02-06 18:57:13 -0300

    termine el carrousel de imagenes yujuu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
-
+let slidderInner = document.getElementById("galeriaimagenes")
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   menuBtn.classList.toggle("active");
@@ -19,3 +19,24 @@ document.addEventListener("click", (e) => {
     menuBtn.textContent = "☰";
   }
 });
+
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel img");
+const dots = document.querySelectorAll(".dot");
+
+let index = 0;
+
+setInterval(() => {
+  index++;
+
+  if (index === slides.length) {
+    index = 0;
+  }
+
+  track.style.transform = `translateX(-${index * 100}%)`;
+
+  // actualizar dots
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[index].classList.add("active");
+
+}, 3000);
