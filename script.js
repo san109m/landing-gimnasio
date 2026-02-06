4de40a67f762fe9f59dc9f353e0d18654d92ccc2
Santiago <santimella11@gmail.com>
2026-02-06 16:32:37 -0300

    termine el navbar responsive y ajuste el hero

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-
 const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 
-
 menuBtn.addEventListener("click", () => {
-navLinks.classList.toggle("active");
+  navLinks.classList.toggle("active");
+  menuBtn.classList.toggle("active");
+
+  menuBtn.textContent = menuBtn.classList.contains("active") ? "✕" : "☰";
+});
+
+
+document.addEventListener("click", (e) => {
+  const clickDentroMenu = navLinks.contains(e.target);
+  const clickEnBoton = menuBtn.contains(e.target);
+
+  if (!clickDentroMenu && !clickEnBoton) {
+    navLinks.classList.remove("active");
+    menuBtn.classList.remove("active");
+    menuBtn.textContent = "☰";
+  }
 });
